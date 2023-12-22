b7409b0463a1bc03c8fd8d572981a1e50d2806a6
Lalo Martins <lalo.martins@gmail.com>
2023-12-22 19:58:14 +0900

    dummy search result item component

---
+++ src/components/common/search/SearchResultItem.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+
+import { SearchResultsContext } from "../../logic/search";
+
+export function SearchResultItem({ result }) {
+  // FIXME
+  const results = useContext(SearchResultsContext);
+
+  return (
+    <div className="search-result-item" key={result.pageid}>
+      <a className="search-result-title" href={results.pageLink(result)}>
+        {result.title}
+      </a>
+      <p className="metadata">
+        Last updated {result.timestamp}; contains {result.wordcount} words
+      </p>
+      <div
+        className="search-result-snippet"
+        dangerouslySetInnerHTML={{
+          __html: result.snippet,
+        }}
+      />
+    </div>
+  );
+}
+export function SearchResultsList({ result }) {
+  const results = useContext(SearchResultsContext);
+
+  return (
+    <div id="search-results-list">
+      {results.query.search.map((result) => (
+        <SearchResultItem result={result} />
+      ))}
+    </div>
+  );
+}
--- src/components/common/search/SearchResultsContainer.jsx
+++ src/components/common/search/SearchResultsContainer.jsx
@@ -1,32 +1,12 @@
-import { useContext } from "react";
-import { SearchResultsContext } from "../../logic/search";
 import { SearchResultsMetadata } from "./SearchResultsMetadata";
 import { SearchResultsPagination } from "./SearchResultsPagination";
+import { SearchResultsList } from "./SearchResultItem";
 
-export function SearchResultsContainer({ result }) {
-  const results = useContext(SearchResultsContext);
-
+export function SearchResultsContainer() {
   return (
     <div id="search-results">
       <SearchResultsMetadata />
-      <div id="search-results-list">
-        {results.query.search.map((result) => (
-          <div className="search-result-item" key={result.pageid}>
-            <a className="search-result-title" href={results.pageLink(result)}>
-              {result.title}
-            </a>
-            <p className="metadata">
-              Last updated {result.timestamp}; contains {result.wordcount} words
-            </p>
-            <div
-              className="search-result-snippet"
-              dangerouslySetInnerHTML={{
-                __html: result.snippet,
-              }}
-            />
-          </div>
-        ))}
-      </div>
+      <SearchResultsList />
       <SearchResultsPagination />
     </div>
   );
