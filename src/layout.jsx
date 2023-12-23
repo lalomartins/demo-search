f45b3398ac303df682aa414dd3192319e7e8ef93
Lalo Martins <lalo.martins@gmail.com>
2023-12-23 19:30:53 +0900

    polymer layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,40 +1,37 @@
 import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
+import "@polymer/app-layout/app-layout.js";
 
 import logo from "./logo.svg";
 
+// eslint-disable-next-line no-unused-vars
 const { Header, Footer, Content } = Layout;
 
 export function MainLayout() {
   return (
-    <Layout>
-      <Header
-        style={{
-          position: "sticky",
-          top: 0,
-          zIndex: 1,
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          style={{ height: 32 }}
-        />
-        <h1 style={{ color: "whitesmoke" }}>React Search Demo</h1>
-      </Header>
-      <Content>
-        <Outlet />
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
+    <>
+      <app-header-layout>
+        <app-header slot="header" fixed effects="waterfall">
+          <app-toolbar>
+            <img
+              src={logo}
+              className="App-logo"
+              alt="logo"
+              style={{ height: 32 }}
+            />
+            <h1 main-title>Lit Search Demo</h1>
+          </app-toolbar>
+        </app-header>
+        <main>
+          <Outlet />
+        </main>
+      </app-header-layout>
+      <footer>
         ©2023 {/* eslint-disable-next-line react/jsx-no-target-blank */}
         <a href="https://lalomartins.info/" target="_blank">
           Lalo Martins
         </a>
-      </Footer>
-    </Layout>
+      </footer>
+    </>
   );
 }
