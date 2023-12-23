4df3ed0e708f18cdb6c2425800f1b4592b0af436
Lalo Martins <lalo.martins@gmail.com>
2023-12-23 17:15:56 +0900

    add lit and shoelace

--- src/components/common/search/SearchResultsContainer.jsx
+++ src/components/common/search/SearchResultsContainer.jsx
@@ -1,4 +1,4 @@
-import { Spin } from "antd";
+import SlSpinner from "@shoelace-style/shoelace/dist/react/spinner";
 
 import { SearchResultsMetadata } from "./SearchResultsMetadata";
 import { SearchResultsPagination } from "./SearchResultsPagination";
@@ -8,7 +8,7 @@ import { Suspense } from "react";
 export function SearchResultsContainer() {
   return (
     <div id="search-results">
-      <Suspense fallback={<Spin size="large" />}>
+      <Suspense fallback={<SlSpinner style={{ fontSize: "3rem" }} />}>
         <SearchResultsMetadata />
         <SearchResultsList />
         <SearchResultsPagination />
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ConfigProvider } from "antd";
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 
 import "./index.css";
 import { MainLayout } from "./layout";
@@ -9,6 +11,10 @@ import { HomePage } from "./components/pages/home";
 import { SearchPage } from "./components/pages/search";
 import { ErrorPage } from "./components/pages/error";
 import reportWebVitals from "./reportWebVitals";
+
+setBasePath(
+  "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/"
+);
 
 const router = createBrowserRouter([
   {
