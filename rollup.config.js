fcfdc15f848d410edfcccf31a0027cfa181c93b3
Lalo Martins <lalo.martins@gmail.com>
2023-12-25 06:15:01 +0900

    copy assets to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
-import html from '@web/rollup-plugin-html';
-import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import esbuild from 'rollup-plugin-esbuild';
-import { generateSW } from 'rollup-plugin-workbox';
-import path from 'path';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import html from "@web/rollup-plugin-html";
+import { importMetaAssets } from "@web/rollup-plugin-import-meta-assets";
+import copy from "rollup-plugin-copy-assets";
+import esbuild from "rollup-plugin-esbuild";
+import { generateSW } from "rollup-plugin-workbox";
+import path from "path";
 
 export default {
-  input: 'index.html',
+  input: "index.html",
   output: {
-    entryFileNames: '[hash].js',
-    chunkFileNames: '[hash].js',
-    assetFileNames: '[hash][extname]',
-    format: 'es',
-    dir: 'build',
+    entryFileNames: "[hash].js",
+    chunkFileNames: "[hash].js",
+    assetFileNames: "[hash][extname]",
+    format: "es",
+    dir: "build",
   },
   preserveEntrySignatures: false,
 
@@ -22,14 +23,14 @@ export default {
     html({
       minify: true,
       injectServiceWorker: true,
-      serviceWorkerPath: 'build/sw.js',
+      serviceWorkerPath: "build/sw.js",
     }),
     /** Resolve bare module imports */
     nodeResolve(),
     /** Minify JS, compile JS to a lower language target */
     esbuild({
       minify: true,
-      target: ['chrome64', 'firefox67', 'safari11.1'],
+      target: ["chrome64", "firefox67", "safari11.1"],
     }),
     /** Bundle assets references via import.meta.url */
     importMetaAssets(),
@@ -37,9 +38,9 @@ export default {
     babel({
       plugins: [
         [
-          require.resolve('babel-plugin-template-html-minifier'),
+          require.resolve("babel-plugin-template-html-minifier"),
           {
-            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
+            modules: { lit: ["html", { name: "css", encapsulation: "style" }] },
             failOnError: false,
             strictCSS: true,
             htmlMinifier: {
@@ -55,17 +56,20 @@ export default {
     }),
     /** Create and inject a service worker */
     generateSW({
-      globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
-      navigateFallback: '/index.html',
+      globIgnores: ["polyfills/*.js", "nomodule-*.js"],
+      navigateFallback: "/index.html",
       // where to output the generated sw
-      swDest: path.join('build', 'sw.js'),
+      swDest: path.join("build", "sw.js"),
       // directory to match patterns against to be precached
-      globDirectory: path.join('build'),
+      globDirectory: path.join("build"),
       // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest}'],
+      globPatterns: ["**/*.{html,js,css,webmanifest}"],
       skipWaiting: true,
       clientsClaim: true,
-      runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
+      runtimeCaching: [{ urlPattern: "polyfills/*.js", handler: "CacheFirst" }],
+    }),
+    copy({
+      assets: ["assets"],
     }),
   ],
 };
